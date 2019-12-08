59cfa5d17ef71987b37df4e0a99dcfa8ece6d995
Natanel Niazoff <nniazoff@icloud.com>
2019-12-08 10:02:08 +0200

    Add platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftNetworking",
+  platforms: [
+    .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6),
+  ],
   products: [
     .library(
       name: "SwiftNetworking",
